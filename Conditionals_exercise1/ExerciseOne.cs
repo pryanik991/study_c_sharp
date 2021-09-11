d793fac7bffe5e608863c13ad2c4992331e38ecd
Ivan Lipsha <ivanlipsha@gmail.com>
2021-09-11 17:17:25 +0300

    testing merge conflicts

--- Conditionals_exercise1/ExerciseOne.cs
+++ Conditionals_exercise1/ExerciseOne.cs
@@ -12,11 +12,12 @@ namespace Conditionals_exercise1
 		{
 			Console.Write("Please enter a number: ");
 			var input = Console.ReadLine();
-			var number = Convert.ToInt32(input);
-			if (number >= 1 && number <= 10)
-				Console.WriteLine("Valid");
-			else
-				Console.WriteLine("Invalid");
+			var number = Convert.ToUInt32(input);
+
+			var result = number >= 1 && number <= 10
+				? "Valid"
+				: "Invalid";
+			Console.WriteLine(result);
 		}
 	}
 }
--- Conditionals_exercise3/ExerciseThree.cs
+++ Conditionals_exercise3/ExerciseThree.cs
@@ -10,16 +10,14 @@ namespace Conditionals_exercise3
 			// is landscape or portrait.
 			
 			Console.Write("Please enter height of an image: ");
-			var height = Convert.ToInt32(Console.ReadLine());
+			var height = Math.Abs(Convert.ToInt32(Console.ReadLine()));
 			Console.Write("Please enter width of an image: ");
-			var width = Convert.ToInt32(Console.ReadLine());
+			var width = Math.Abs(Convert.ToInt32(Console.ReadLine()));
 
-			if (height > width)
-				Console.WriteLine("Image is a portrait");
-			else if (height < width)
-				Console.WriteLine("Image is a landscape");
-			else
-				Console.WriteLine("Image is in a shape of square");
+			var imageType = height > width
+				? "portrait"
+				: "landscape";
+			Console.WriteLine($"Image is a {imageType}"); //testing merge conflicts tool
 		}
 	}
 }
